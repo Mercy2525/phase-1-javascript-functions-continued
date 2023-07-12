bcf7a51f81ff8d8239c07b01aa20974f540a978a
Mercy2525 <mercywmuriithi.mm@gmail.com>
2023-07-12 12:21:47 +0300

    functions done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog")
+
+
+const mondayWork=function (activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+mondayWork("work from home")
+
+function wrapAdjective(hey) {
+  const emphatic= function (adjective="a hard worker") {
+    
+    return `You are ${hey}${adjective}${hey}!`
+   }
+   return emphatic;
+   
+}
+wrapAdjective("*")
+wrapAdjective("||")("a dedicated programmer")
